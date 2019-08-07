369b8cabcf9de0a1c80782ea343769c7bf0efbbb
ImsirovicAjdin <ajdika@live.com>
2019-08-07 15:47:25 +0200

    setup Source Observable with from()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, from } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+
+  ngOnInit() {
+
+    from([20, 15, 10, 5]).subscribe(
+      item => console.log(`resulting item .. ${item}`),
+      err => console.log(`error occured .. ${err}`),
+      () => console.log('complete')
+    )
+    
+  }
 }
